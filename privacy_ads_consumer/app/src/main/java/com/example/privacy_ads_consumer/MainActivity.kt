0b5c97e5a35b1735ab381b034cb6ff541ba4138f
Matt Boetger <boetger@google.com>
2025-08-28 10:57:07 -0700

    use topics API

--- privacy_ads_consumer/app/src/main/java/com/example/privacy_ads_consumer/MainActivity.kt
+++ privacy_ads_consumer/app/src/main/java/com/example/privacy_ads_consumer/MainActivity.kt
@@ -1,47 +1,102 @@
 package com.example.privacy_ads_consumer
 
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.os.ext.SdkExtensions
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.coroutineScope
-import androidx.privacysandbox.sdkruntime.client.SdkSandboxManagerCompat
+import androidx.privacysandbox.ads.adservices.topics.GetTopicsRequest
+import androidx.privacysandbox.ads.adservices.topics.TopicsManager
 import com.example.privacy_ads_consumer.ui.theme.Privacy_ads_consumerTheme
 import kotlinx.coroutines.launch
 
 private const val TAG = "privacy_ads_consumer"
 
 class MainActivity : ComponentActivity() {
+    private var topicsManager: TopicsManager? = null
+    private lateinit var topicsRequestBuilder:  GetTopicsRequest.Builder
+    // State variable to hold the topics string for the UI
+    private var topicsResultText by mutableStateOf("") // Initialize with default text
+
+    private fun initTopics() {
+        if (SDK_INT >= Build.VERSION_CODES.R && // The extensions API is available since R.
+            SdkExtensions.getExtensionVersion(SdkExtensions.AD_SERVICES) >= 4
+        ) {
+            // The initialization function will return null if the requested
+            // functionality is not available on the device.
+            topicsManager = TopicsManager.obtain(baseContext)
+            if (topicsManager == null) {
+                topicsResultText = "Could not get topics manager"
+                Log.e(TAG, "Could not get topics manager")
+            } else {
+                val shouldRecordObservation = false
+                topicsRequestBuilder  = GetTopicsRequest.Builder()
+                topicsRequestBuilder.setAdsSdkName(baseContext.packageName)
+                topicsRequestBuilder.setShouldRecordObservation(shouldRecordObservation)
+            }
+        } else {
+            topicsResultText = "Could not get topics manager"
+            Log.e(TAG, "Privacy Sandbox Ads API not available.")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initTopics()
         enableEdgeToEdge()
         setContent {
             Privacy_ads_consumerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Button(
-                        onClick = {
-                            lifecycle.coroutineScope.launch {
-                                Log.e(TAG, "onCreate: Calling SDK")
-                                val remoteSdk = SdkSandboxManagerCompat.from(this@MainActivity).loadSdk(
-                                    sdkName = "androidx.privacysandbox.ads.adservices",
-                                    params = Bundle.EMPTY
-                                )
-                                Log.e(TAG, "onCreate: SDK Loaded: $remoteSdk")
-                            }
-                        },
-                        modifier = Modifier.padding(innerPadding),
-                    ) {
-                        Text("Load SDK")
+                    Column(modifier = Modifier.padding(innerPadding).fillMaxSize().padding(16.dp)){
+                        Button(
+                            onClick = {
+                                lifecycle.coroutineScope.launch {
+                                    try {
+                                        val result = topicsManager?.getTopics(
+                                            topicsRequestBuilder.build(),
+                                        )
+                                        if (result != null) {
+                                            val topicsResult = result.topics
+                                            if (topicsResult.isEmpty()) {
+                                                topicsResultText = "Returned Empty"
+                                                Log.i("Topic", "Returned Empty")
+                                            } else {
+                                                topicsResultText = ""
+                                                for (i in topicsResult.indices) {
+                                                    topicsResultText += topicsResult[i].topicId.toString()
+                                                    Log.i(
+                                                        "Topic",
+                                                        topicsResult[i].topicId.toString()
+                                                    )
+                                                }
+                                            }
+                                        }
+                                    } catch (ex: SecurityException) {
+                                        topicsResultText = "$ex"
+                                        Log.e(TAG, "Security Exception", ex)
+                                    }
+                                }
+                            },
+                            modifier = Modifier.padding(innerPadding),
+                        ) {
+                            Text("Get Topics")
+                        }
+                        Text(topicsResultText)
                     }
                 }
             }
--- privacy_ads_consumer/settings.gradle.kts
+++ privacy_ads_consumer/settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "privacy_ads_consumer"
 include(":app")
- 
